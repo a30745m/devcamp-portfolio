6bfac967117bf6744f3cd2f7795eb323999aa239
ediliu <a30745m@gmail.com>
2018-05-07 22:20:20 +0800

    Added controller concerns for devise params

---
+++ app/models/concerns/devise_whitelist.rb
@@ -0,0 +1,17 @@
+# moduel name should be same as file name
+# ex. devise_white_list => DeviseWhiteList
+# ex. devisewhitelist => Devisewhitelist
+module DeviseWhitelist
+  extend ActiveSupport::Concern
+
+  included do
+    before_action :configure_permitted_parameters, if: :devise_controller?
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys:[:name])
+    devise_parameter_sanitizer.permit(:accout_update, keys:[:name])
+  end
+
+end
+
