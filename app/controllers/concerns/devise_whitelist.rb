3f8b188b3c8b2b28331112bb94bdc635d95af5ca
ediliu <a30745m@gmail.com>
2018-05-10 20:46:00 +0800

    Completed authentication with virtual attributes for user model and fixed previous typo

--- app/controllers/concerns/devise_whitelist.rb
+++ app/controllers/concerns/devise_whitelist.rb
@@ -9,9 +9,7 @@ module DeviseWhitelist
   end
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys:[:name])
-    devise_parameter_sanitizer.permit(:accout_update, keys:[:name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
   end
-
 end
-
