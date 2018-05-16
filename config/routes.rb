5d7b89931f0606a77bb527df2681b35077b7c092
ediliu <a30745m@gmail.com>
2018-05-16 22:01:37 +0800

    Added nav bar and added default header content

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
 
   # able to create the name you love
-  get 'about', to: 'pages#about'
+  get 'about_me', to: 'pages#about'
   get 'contact', to: 'pages#contact'
 
   resources :blogs do
